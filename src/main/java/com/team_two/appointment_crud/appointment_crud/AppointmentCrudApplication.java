928b5bd54c2271d343700f37a373b20586bfbe3e
sirlocust <jua56yu@hotmail.com>
2021-09-14 14:44:45 -0500

    Se crear proyecto de spring con maven maven dependencias spring Web JPA MySql connector sprintboot Tools

---
+++ src/main/java/com/team_two/appointment_crud/appointment_crud/AppointmentCrudApplication.java
@@ -0,0 +1,13 @@
+package com.team_two.appointment_crud.appointment_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team_two/appointment_crud/appointment_crud/AppointmentCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_two.appointment_crud.appointment_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
