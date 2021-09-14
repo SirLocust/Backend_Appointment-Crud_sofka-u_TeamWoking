b8e04e250d3a41f25bf429133e06308eebc08080
Jhon Serna <jhonalfaber@gmail.com>
2021-09-14 17:30:37 -0500

    Fixing names errors in ServiceAppointmentImpl

--- src/main/java/com/team_two/appointment_crud/appointment_crud/service/ServiceAppointmentImpl.java
+++ src/main/java/com/team_two/appointment_crud/appointment_crud/service/ServiceAppointmentImpl.java
@@ -1,14 +1,16 @@
 package com.team_two.appointment_crud.appointment_crud.service;
-
+import com.team_two.appointment_crud.appointment_crud.entity.Appointment;
+import com.team_two.appointment_crud.appointment_crud.service.ServiceAppointment;
+import com.team_two.appointment_crud.appointment_crud.Repository.RepositoryAppointment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class ServiceAppointment1 implements ServiceAppointmentImpl {
+public class ServiceAppointmentImpl implements ServiceAppointment{
     @Autowired
-    private ServiceAppointmentImpl service;
+    private RepositoryAppointment service;
     @Override
     public List<Appointment> ReadAll(){
         return (List<Appointment>) service.findAll();
@@ -17,7 +19,6 @@ public class ServiceAppointment1 implements ServiceAppointmentImpl {
     public Appointment ReadId(Long id){
         return service.findById(id).get();
     }
-
     @Override
     public Appointment Save(Appointment appointment){
         return service.save(appointment);
