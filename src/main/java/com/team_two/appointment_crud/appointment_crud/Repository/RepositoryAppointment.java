8a3ee2f00c2a72db8dc8536c1c6127d2758aa295
arimohy <arimohy@gmail.com>
2021-09-14 17:46:09 -0500

    controller package creation

--- src/main/java/com/team_two/appointment_crud/appointment_crud/Repository/RepositoryAppointment.java
+++ src/main/java/com/team_two/appointment_crud/appointment_crud/Repository/RepositoryAppointment.java
@@ -2,6 +2,8 @@ package com.team_two.appointment_crud.appointment_crud.Repository;
 
 import com.team_two.appointment_crud.appointment_crud.entity.Appointment;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositoryAppointment extends CrudRepository<Appointment,Long> {
 }
